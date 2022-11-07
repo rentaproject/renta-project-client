4a6749aaa0d5916f6a31844fff9c4f5f987bcdfa
billhikmah <billhikmah1@yahoo.co.id>
2022-11-08 05:29:03 +0700

    Fix Bug

--- utilities/axiosClient.jsx
+++ utilities/axiosClient.jsx
@@ -32,7 +32,7 @@ axiosApiIntances.interceptors.response.use(
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
 
-    if (error.response.status === 403) {
+    if (error.response.data.status === 403) {
       Cookies.remove("token");
       window.location.href = "/auth/login";
     }
