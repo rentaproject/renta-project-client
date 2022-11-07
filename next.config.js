69016f01a08809b619f4cf2b59f7a29b7c921f3d
billhikmah <billhikmah1@yahoo.co.id>
2022-11-07 21:02:41 +0700

    Change Url Cloudinary

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
   env: {
     URL_BACKEND: "https://renta-project-server.vercel.app",
     URL_CLOUDINARY:
-      "https://res.cloudinary.com/dnhoxflfj/image/upload/v1667588114/",
+      "https://res.cloudinary.com/dnhoxflfj/image/upload/v1667823115/",
   },
 };
 const withImages = require("next-images");
